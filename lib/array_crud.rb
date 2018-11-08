109b47464675d4b002ab8614e6b34766ce0a2b69
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2018-11-08 01:42:08 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -26,9 +26,9 @@ def remove_element_from_start_of_array(array)
   array.shift("wow")
 end
 
-def retrieve_element_from_index(array, index number)
+def retrieve_element_from_index(array, index_number)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
-  array[index number]
+  array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
