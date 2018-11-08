e37abfc3539c1d02374ad94dd205ab543e44a84e
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2018-11-08 01:36:05 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,7 +12,7 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  array = [1, 2, 3, 4]
+  array = ["wow", "I", "am", "really", "learning"]
   array.unshift(0)
 end
 
