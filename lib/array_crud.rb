55ba30bc814751a2ed030ecaad824248327f5a89
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2018-11-08 01:36:43 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,12 +12,12 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  array = ["wow", "I", "am", "really", "learning"]
-  array.unshift(0)
+  array = ["I", "am", "really", "learning"]
+  array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  array = [1, 2, 3, 4]
+  array = ["I", "am", "really",]
   array.pop(4)
 end
 
